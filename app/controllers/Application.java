39dda008f542a30445d72a1f39b5936f4b84ef30
megan-c-hill <meganchill@iastate.edu>
2017-09-26 17:43:38 -0500

    Setting up Letter class and conversions

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,10 +6,13 @@ import play.mvc.*;
 import java.util.*;
 
 import models.*;
+import models.Letter;
 
 public class Application extends Controller {
 
     public static void index() {
+        Letter A = new Letter(65);
+        Letter a = new Letter(97);
         render();
     }
 
--- app/models/Letter.java
+++ app/models/Letter.java
@@ -1,16 +1,31 @@
 package models;
 
 public class Letter{
-    char letter = "";
+    char letter = '0';
     int numEquiv = 0;
 
+    public Letter(String letter){
+        this.letter = letter.charAt(0);
+        setNum();
+    }
     public Letter(char letter){
         this.letter = letter;
         setNum();
     }
+    public Letter(int numEquiv){
+        this.numEquiv = numEquiv;
+        setLetter();
+    }
 
     private void setNum(){
+        letter = Character.toLowerCase(letter);
+        numEquiv = (int)letter;
+        numEquiv -= 96;
 
+    }
+    private void setLetter(){
+        letter = (char)numEquiv;
+        letter = Character.toLowerCase(letter);
     }
 
 
