39eeef755675378b61cccd380f7e9d057c653f0f
megan-c-hill <meganchill@iastate.edu>
2017-09-26 17:23:06 -0500

    Initial Commit

---
+++ app/models/Letter.java
@@ -0,0 +1,17 @@
+package models;
+
+public class Letter{
+    char letter = "";
+    int numEquiv = 0;
+
+    public Letter(char letter){
+        this.letter = letter;
+        setNum();
+    }
+
+    private void setNum(){
+
+    }
+
+
+}
