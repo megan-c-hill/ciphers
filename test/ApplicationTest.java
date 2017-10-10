65ed32ea08f848b9f28170612a8c62eaf2b60d5c
megan-c-hill <meganchill@iastate.edu>
2017-10-10 15:40:42 -0500

    Added some application tests

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -13,5 +13,19 @@ public class ApplicationTest extends FunctionalTest {
         assertContentType("text/html", response);
         assertCharset(play.Play.defaultWebEncoding, response);
     }
+
+    @Test
+    public void testThatEncryptPageWorks() {
+        Response response = GET("/encrypt");
+        assertIsOk(response);
+        assertContentType("text/html", response);
+    }
+
+    @Test
+    public void testThatDecipherPageWorks() {
+        Response response = GET("/decipher");
+        assertIsOk(response);
+        assertContentType("text/html", response);
+    }
     
 }
